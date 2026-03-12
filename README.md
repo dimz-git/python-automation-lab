28333540e312c4e081836747e4a7f526f914c87d
Dmazfr <ramadhanfajridimas@gmail.com>
2026-03-12 15:40:40 +0700

    Refactor: Add functions to price filter

---
+++ README.md
@@ -0,0 +1,6 @@
+Title: Price Filter Automation
+The Problem: E-commerce owners spend hours manually sorting product prices to find budget-friendly items.
+The Solution: This Python script automatically categorizes products as 'Affordable' or 'Premium' based on custom price thresholds.
+How to use:
+Change the product_price list with your own inventory data.
+Run the script and get instant insights.
---
+++ price_filter.py
@@ -0,0 +1,29 @@
+#function for categorization the price
+def categorization_price(price_list, threshold=500):
+    for price in price_list : 
+        if price > threshold:
+            print(f"premium: {price}")
+        else:
+            print(f"affordable: {price}")
+#function for apply_discount
+def apply_discount(price_list):
+    for price in price_list:
+        if price > 1000:
+            new_price = price *0.9
+            print(f"special pricer (10% Off): {new_price}")
+        else:
+            print(f"normal price: {price}")
+#the data that can you change           
+my_data = [1222,3444,5444,123,2123]
+print("--- Categorization ---")
+categorization_price(my_data)
+print("\n--- Discount Applied ---")
+apply_discount(my_data)
+
+
+
+
+
+
+
+
